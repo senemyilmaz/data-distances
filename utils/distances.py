84f092e1163794bf770cae26cbe70255b2ca2002
Senem Yilmaz <senemyilmaaz@outlook.com>
2026-01-15 05:27:39 +0300

    complated

--- utils/distances.py
+++ utils/distances.py
@@ -1,10 +1,14 @@
-def manhattan(a,b):
-    d_x = b[0] - a[0]
-    d_y = b[1] - a[1]
-    return abs(d_x) + abs(d_y)
+def manhattan(a, b):
+    return minkowski(a, b, 1)
+
+
 def euclidean(a, b):
+    return minkowski(a, b, 2)
+
+
+def minkowski(a, b, p):
     d_x = b[0] - a[0]
     d_y = b[1] - a[1]
 
-    distance = d_x**2 + d_y**2
-    return distance ** 0.5
+    distance = (abs(d_x)**p + abs(d_y)**p)**(1/p)
+    return distance
